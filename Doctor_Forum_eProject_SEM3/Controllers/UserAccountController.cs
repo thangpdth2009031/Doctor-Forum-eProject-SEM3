3d4f4a0e53c8af5fd5220ce9fcd5115be78249aa
thangpdth2009031@fpt.edu.vn <thangpdth2009031@fpt.edu.vn>
2022-05-05 08:06:44 +0700

    update-title

--- Doctor_Forum_eProject_SEM3/Controllers/UserAccountController.cs
+++ Doctor_Forum_eProject_SEM3/Controllers/UserAccountController.cs
@@ -152,7 +152,7 @@ namespace Doctor_Forum_eProject_SEM3.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(LoginModel model)
+        public ActionResult Login(LoginModel model)     
         {
             if (ModelState.IsValid)
             {
